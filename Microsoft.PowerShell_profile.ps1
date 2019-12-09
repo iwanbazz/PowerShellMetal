6666ab542bc5e49f2834a6390b33aa1343b3f984
iwanbazz <iwan.bazz@gmail.com>
2019-12-09 23:14:21 +0700

    Using root wsl

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -17,13 +17,14 @@ if (Test-Path($ChocolateyProfile)) {
 
 # Alias
 function art() {php artisan $args}
-function cdh() {cd C:\Users\Iwan}
+function cdh() {Set-Location C:\Users\Iwan}
 function cu() {composer update}
+function e.() {explorer .}
 function fed() {sudo Start-VM -Name Fedora}
-function install() {sudo choco install -y $args}
+function ins() {sudo choco install -y $args}
 function lnew() {laravel new $args}
 function rh() {sudo Start-VM -Name Redhat}
-function up() {sudo Install-WindowsUpdate; sudo choco upgrade all -y; scoop update *; composer global update; npm update -g}
+function up() {sudo Install-WindowsUpdate -AcceptAll; sudo choco upgrade all -y; wsl -u root apt update; wsl -u root apt upgrade -y; scoop update *; flutter upgrade; composer global update; npm update -g}
 
 # Alias other
 function wa() {l wsl-alias add $args}
@@ -31,21 +32,25 @@ function wl() {l wsl-alias list}
 function wr() {l wsl-alias remove $args}
 
 # Alias wsl
-function mstart() {wsl sudo service mongodb start}
-function mstat() {wsl sudo service mongodb status}
-function mstop() {wsl sudo service mongodb stop}
-function nstart() {wsl sudo service nginx start}
-function nstat() {wsl sudo service nginx status}
-function nstop() {wsl sudo service nginx stop}
-function pgstart() {wsl sudo service postgresql start}
-function pgstat() {wsl sudo service postgresql status}
-function pgstop() {wsl sudo service postgresql stop}
-function pstart() {wsl sudo service php7.3-fpm start}
-function pstat() {wsl sudo service php7.3-fpm status}
-function pstop() {wsl sudo service php7.3-fpm stop}
-function sqlstart() {wsl sudo service mysql start}
-function sqlstat() {wsl sudo service mysql status}
-function sqlstop() {wsl sudo service mysql stop}
+function d() {wsl -d Debian -u root $args}
+function f() {wsl -d fedoraremix -u root $args}
+function k() {wsl -d kali -u root $args}
+function u() {wsl -d ubuntu -u root $args}
+function mstart() {wsl -u root service mongodb start}
+function mstat() {wsl -u root service mongodb status}
+function mstop() {wsl -u root service mongodb stop}
+function nstart() {wsl -u root service nginx start}
+function nstat() {wsl -u root service nginx status}
+function nstop() {wsl -u root service nginx stop}
+function pgstart() {wsl -u root service postgresql start}
+function pgstat() {wsl -u root service postgresql status}
+function pgstop() {wsl -u root service postgresql stop}
+function pstart() {wsl -u root service php7.4-fpm start}
+function pstat() {wsl -u root service php7.4-fpm status}
+function pstop() {wsl -u root service php7.4-fpm stop}
+function sqlstart() {wsl -u root service mysql start}
+function sqlstat() {wsl -u root service mysql status}
+function sqlstop() {wsl -u root service mysql stop}
 function wstart() {nstart; pstart}
 function wstop() {nstop; pstop}
 function lstart() {wstart; sqlstart}
