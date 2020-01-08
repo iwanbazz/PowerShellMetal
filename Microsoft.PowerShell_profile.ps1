25dd43ce17fbcc93566ca43f1eee95a25c91f28e
iwanbazz <iwan.bazz@gmail.com>
2020-01-08 08:21:25 +0700

    Update 2020

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,12 @@
-# ~\Documents\PowerShell\Profile.ps1
-# Windows PowerShell
 Invoke-Expression (&starship init powershell)
 
 Import-Module DockerCompletion
 #Import-Module oh-my-posh
-#Import-Module posh-git
+Import-Module posh-git
 Import-Module PSReadLine
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+Import-Module PSFzf
+Import-Module PSScheduledJob
 Import-Module PSWindowsUpdate
 #Set-Theme Paradox
 
@@ -19,12 +20,19 @@ if (Test-Path($ChocolateyProfile)) {
 function art() {php artisan $args}
 function cdh() {Set-Location C:\Users\Iwan}
 function cu() {composer update}
+function dock() {wsl -d Ubuntu docker $args}
 function e.() {explorer .}
 function fed() {sudo Start-VM -Name Fedora}
+function gc() {git clone $args}
+function gl() {git pull}
 function ins() {sudo choco install -y $args}
+function iwando() {ssh iwan@134.209.101.188}
+function lup() {npm install; npm run dev; composer install}
 function lnew() {laravel new $args}
 function rh() {sudo Start-VM -Name Redhat}
+function rootdo() {ssh root@134.209.101.188}
 function up() {sudo Install-WindowsUpdate -AcceptAll; sudo choco upgrade all -y; wsl -u root apt update; wsl -u root apt upgrade -y; scoop update *; flutter upgrade; composer global update; npm update -g}
+function x() {exit}
 
 # Alias other
 function wa() {l wsl-alias add $args}
@@ -34,11 +42,14 @@ function wr() {l wsl-alias remove $args}
 # Alias wsl
 function d() {wsl -d Debian -u root $args}
 function f() {wsl -d fedoraremix -u root $args}
-function k() {wsl -d kali -u root $args}
+function k() {wsl -d kali-linux -u root $args}
 function u() {wsl -d ubuntu -u root $args}
-function mstart() {wsl -u root service mongodb start}
-function mstat() {wsl -u root service mongodb status}
-function mstop() {wsl -u root service mongodb stop}
+function dstart() {wsl -d Ubuntu -u root service docker start}
+function dstat() {wsl -d Ubuntu -u root service docker status}
+function dstop() {wsl -d Ubuntu -u root service docker stop}
+function mstart() {wsl -d Ubuntu -u root service mongodb start}
+function mstat() {wsl -d Ubuntu -u root service mongodb status}
+function mstop() {wsl -d Ubuntu -u root service mongodb stop}
 function nstart() {wsl -u root service nginx start}
 function nstat() {wsl -u root service nginx status}
 function nstop() {wsl -u root service nginx stop}
